37f894652f5e5e4f520bf93ada41665211ee6d84
Matheus Almeida <matheusalmeida2167@gmail.com>
2023-11-25 16:34:49 -0400

    Atualização

--- static/script.js
+++ static/script.js
@@ -1,33 +1,23 @@
 function getRecommendations() {
     var bookID = document.getElementById("bookID").value;
 
-    // Adicione uma classe de animação enquanto as recomendações estão sendo carregadas
     document.getElementById("recommendations").classList.add("loading");
 
-    // Enviar solicitação para o backend (Flask)
     fetch(`/recommender?bookID=${bookID}`)
     .then(response => response.json())
     .then(data => {
         console.log("Data Received:", data);
-        
-        // Remova a classe de animação após o recebimento dos dados
         document.getElementById("recommendations").classList.remove("loading");
-
-        // Atualize as informações com uma transição suave
         updateRecommendationUI(data);
     })
     .catch(error => {
         console.error('Erro na solicitação:', error);
-
-        // Remova a classe de animação em caso de erro
         document.getElementById("recommendations").classList.remove("loading");
-
         document.getElementById("recommendations").innerHTML = 'Erro ao obter recomendações.';
     });
 }
 
 function updateRecommendationUI(data) {
-    // Adicione lógica para exibir informações com animações ou transições
     document.getElementById("recommendations").innerHTML = `
         <p>Recommended Books:</p>
         <ul>
